e74596ec75db635b3eddededb96d21b1a0bc13f5
vijay sharma <vijaysharma@snakescript.com>
2024-04-24 17:18:39 +0530

    update message using recived

--- socketapp/consumers.py
+++ socketapp/consumers.py
@@ -2,7 +2,9 @@
 
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+import requests
 
+#example for sending message using server side
 class GetPriceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = "price_updates"  # Define the group name
@@ -21,4 +23,41 @@ class GetPriceConsumer(AsyncWebsocketConsumer):
         message = event["message"]
         await self.send(text_data=json.dumps({
             'message': message
+        }))
+
+
+
+#sending message using client side
+
+class ChatBotConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.group_name ="chat"
+        await self.channel_layer.group_add(self.group_name,self.channel_name)
+        await self.accept()
+
+    async def disconnect(self,close_code):
+        await self.channel_layer.group_discard(self.group_name,self.channel_name)
+
+    async def receive(self,text_data):
+        print(text_data)
+        data = json.loads(text_data)
+
+        # Extract the user message from the received data
+        user_message = data.get('message')
+        #handle recived data
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type':'chat_message',
+                'message':user_message
+            }
+        )
+
+    async def chat_message(self,event):
+        message = event['message']
+
+
+        await self.send(text_data=json.dumps({
+            'message':message
         }))
--- socketapp/routing.py
+++ socketapp/routing.py
@@ -1,8 +1,10 @@
 # routing.py
 
 from django.urls import path
-from .consumers import GetPriceConsumer
+from .consumers import GetPriceConsumer,ChatBotConsumer
 
 websocket_urlpatterns = [
     path('ws/get_price/', GetPriceConsumer.as_asgi()),
+    path('ws/chat/', ChatBotConsumer.as_asgi()),
+
 ]
